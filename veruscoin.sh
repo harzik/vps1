a417bfa5fe32dace796b2164eaa377307a6e615f
harzik <85394651+harzik@users.noreply.github.com>
2021-06-06 02:25:21 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
 
 ! wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 ! tar -xvf hellminer_cpu_linux.tgz
