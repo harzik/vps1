db5db45423674aa48d2a857781c2365e72251e60
harzik <85394651+harzik@users.noreply.github.com>
2021-06-06 02:10:13 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-#!/bin/bash
+
 #
 
 sudo apt update
-
+sudo apt install python
 sudo apt install screen -y
 
 # Download File
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-# extrak file
-tar xf hellminer_cpu_linux.tar.gz
+! wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+! tar -xvf hellminer_cpu_linux.tgz
+! tar -xvf hellminer_cpu_linux.tar.gz
 #Run Mining
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9TbesCmDoX4SGP174E27bS1xykMRsDwie.ted1 -p x --cpu 2
 while [ 1 ]; do
